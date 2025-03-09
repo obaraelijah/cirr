c718edda3135b5d46d50a81da5db1ffc1a9b5611
obaraelijah <elijahobara357@gmail.com>
2025-03-09 16:03:56 +0300

    feat: init

--- cmd/root.go
+++ cmd/root.go
@@ -2,22 +2,50 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var cfgFile string
 
 var rootCmd = &cobra.Command{
 	Use:   "cirr",
-	Short: "cirr retrieves cloud IP ranges from various providers",
-	Long:  `A CLI tool to fetch and display IP ranges from cloud providers like AWS, Azure, and Google Cloud.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Welcome to cirr! Use --help for available commands.")
-	},
+	Short: "",
+	Long:  ``,
 }
 
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		return
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cirr.yaml)")
+
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".cirr")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 	}
 }
