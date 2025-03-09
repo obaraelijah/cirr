a4e44c1c752ac99c8df28c7bfb3dd5e9fd665d45
obaraelijah <elijahobara357@gmail.com>
2025-03-09 16:40:07 +0300

    feat: add filter support, filter splitter and ip ranges data main filter func

--- cmd/aws.go
+++ cmd/aws.go
@@ -6,6 +6,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	awsIPv4Flag     bool
+	awsIPv6Flag     bool
+	awsIPFilterFlag string
+)
+
 var awsCmd = &cobra.Command{
 	Use:   "aws",
 	Short: "Get AWS IP ranges.",
@@ -15,11 +21,11 @@ var awsCmd = &cobra.Command{
 
 		logger.Println("AWS subcommand called")
 
-		if ipv4Flag || (!ipv4Flag && !ipv6Flag) {
-			aws.GetIPRanges("ipv4")
+		if awsIPv4Flag || (!awsIPv4Flag && !awsIPv6Flag) {
+			aws.GetIPRanges("ipv4", awsIPFilterFlag)
 		}
-		if ipv6Flag || (!ipv4Flag && !ipv6Flag) {
-			aws.GetIPRanges("ipv6")
+		if awsIPv6Flag || (!awsIPv4Flag && !awsIPv6Flag) {
+			aws.GetIPRanges("ipv6", awsIPFilterFlag)
 		}
 	},
 }
@@ -27,6 +33,7 @@ var awsCmd = &cobra.Command{
 func main() {
 	rootCmd.AddCommand(awsCmd)
 
-	awsCmd.Flags().BoolVar(&ipv4Flag, "ipv4", false, "Get only IPv4 ranges")
-	awsCmd.Flags().BoolVar(&ipv6Flag, "ipv6", false, "Get only IPv6 ranges")
+	awsCmd.Flags().BoolVar(&awsIPv4Flag, "ipv4", false, "Get only IPv4 ranges")
+	awsCmd.Flags().BoolVar(&awsIPv6Flag, "ipv6", false, "Get only IPv6 ranges")
+	awsCmd.Flags().StringVar(&awsIPFilterFlag, "filter", "", "Filter results. Syntax: aws-region-az,SERVICE,network-border-group")
 }
