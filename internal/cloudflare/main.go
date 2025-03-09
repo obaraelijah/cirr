969b48933abfcf1e84fad416cd6ae2356918c7f5
obaraelijah <elijahobara357@gmail.com>
2025-03-09 16:13:37 +0300

    feat: add calls for ipv4 & ipv6

---
+++ internal/cloudflare/main.go
@@ -0,0 +1,19 @@
+package cloudflare
+
+import (
+	"fmt"
+
+	"github.com/obaraelijah/cirr/internal/utils"
+)
+
+func GetCloudflareIPv4Ranges() {
+	data := utils.GetReq("https://www.cloudflare.com/ips-v4/")
+
+	fmt.Println(data)
+}
+
+func GetCloudflareIPv6Ranges() {
+	data := utils.GetReq("https://www.cloudflare.com/ips-v6/")
+
+	fmt.Println(data)
+}
