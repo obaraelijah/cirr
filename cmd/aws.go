c0be6cd0c1830a9fc0aee30259296c41676b2e88
obaraelijah <elijahobara357@gmail.com>
2025-03-10 14:30:02 +0300

    feat:add verbosity flag to aws subcommand

--- cmd/aws.go
+++ cmd/aws.go
@@ -10,6 +10,7 @@ var (
 	awsIPv4Flag     bool
 	awsIPv6Flag     bool
 	awsIPFilterFlag string
+	awsVerbosity    string
 )
 
 var awsCmd = &cobra.Command{
@@ -22,10 +23,10 @@ var awsCmd = &cobra.Command{
 		logger.Println("AWS subcommand called")
 
 		if awsIPv4Flag || (!awsIPv4Flag && !awsIPv6Flag) {
-			aws.GetIPRanges("ipv4", awsIPFilterFlag, utils.GetReq)
+			aws.GetIPRanges("ipv4", awsIPFilterFlag, awsVerbosity, utils.GetReq)
 		}
 		if awsIPv6Flag || (!awsIPv4Flag && !awsIPv6Flag) {
-			aws.GetIPRanges("ipv6", awsIPFilterFlag, utils.GetReq)
+			aws.GetIPRanges("ipv6", awsIPFilterFlag, awsVerbosity, utils.GetReq)
 		}
 	},
 }
@@ -36,4 +37,5 @@ func init() {
 	awsCmd.Flags().BoolVar(&awsIPv4Flag, "ipv4", false, "Get only IPv4 ranges")
 	awsCmd.Flags().BoolVar(&awsIPv6Flag, "ipv6", false, "Get only IPv6 ranges")
 	awsCmd.Flags().StringVar(&awsIPFilterFlag, "filter", "", "Filter results. Syntax: aws-region-az,SERVICE,network-border-group")
+	awsCmd.Flags().StringVar(&awsVerbosity, "verbose", "none", "Verbosity. Options: none, mini, full")
 }
